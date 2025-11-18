21f4e2a64b6467539ba7998252e4c985ca7c664a
abhinavgkrishnan <abhinavgkrishnan@gmail.com>
2025-11-18 22:17:42 +0530

    typefix for zinger shuffle

--- components/game-over.tsx
+++ components/game-over.tsx
@@ -7,7 +7,7 @@ import { Trophy, Target, Zap, RotateCcw, Home, Swords, Share2, ArrowLeft } from 
 import { GAME_CONFIG, TEXT } from "@/lib/constants"
 
 // Shuffle array helper
-const shuffleArray = <T,>(array: T[]): T[] => {
+const shuffleArray = <T,>(array: readonly T[]): T[] => {
   const shuffled = [...array]
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -17,7 +17,7 @@ const shuffleArray = <T,>(array: T[]): T[] => {
 }
 
 // Get random item from array
-const getRandomItem = <T,>(array: T[]): T => {
+const getRandomItem = <T,>(array: readonly T[]): T => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
